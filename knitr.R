798bf0984150337afe9d30e2f8f69a8f25c5602f
Adam Marton <adam@adammarton.com>
2024-06-28 17:32:04 -0400

    knitr

---
+++ knitr.R
@@ -0,0 +1,4 @@
+install.packages("knitr")
+knitr::purl(input = "/Users/adammarton/Documents/GitHub/election-test-scraper-2024/election-test-scraper-2024.Rmd", output = "/Users/adammarton/Documents/GitHub/election-test-scraper-2024/election-test-scraper-2024.R",documentation = 0)
+
+

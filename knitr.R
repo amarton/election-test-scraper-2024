2143acef1d04a3a3c3e35af6801e509ca5679001
Adam Marton <adam@adammarton.com>
2024-07-02 13:36:09 -0400

    remove commas from results

--- knitr.R
+++ knitr.R
@@ -2,3 +2,4 @@ install.packages("knitr")
 knitr::purl(input = "/Users/adammarton/Documents/GitHub/election-test-scraper-2024/election-test-scraper-2024.Rmd", output = "/Users/adammarton/Documents/GitHub/election-test-scraper-2024/election-test-scraper-2024.R",documentation = 0)
 
 
+
